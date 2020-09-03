e3a9fdc6ab1bfe0cccff9b2db123177b8b9183e2
thuanqt <thuanqt@uio.no>
2020-09-03 23:13:37 +0200

    Finished

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
-def add(x,y):
+def add(x, y):
     return x + y
+
 
 def factorial(n):
     if n == 0:
@@ -9,17 +10,21 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
-def sin(x,N):
+
+def sin(x, N):
     sum = 0
-    for n in range(0,N):
+    for n in range(0, N):
         sum += ((-1)**n*x**(2*n+1))/(factorial(2*n+1))
     return sum
 
-def divide(x,y):
+
+def divide(x, y):
     return x/y
 
-def multiplication(x,y):
+
+def multiplication(x, y):
     return x*y
 
-def subtraction(x,y):
+
+def subtraction(x, y):
     return x - y
